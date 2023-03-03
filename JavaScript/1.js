68ebf314744768e4d1481889fc338d8122f20a45
Alina <AliKochneva@yandex.ru>
2023-03-03 11:54:47 +0700

    changes

--- JavaScript/1.js
+++
@@ -1,7 +0,0 @@
-
-
-
-const tempC = parseInt(prompt("Введите температуру в Цельсиях: "));
-const tempF = (9 / 5) * tempC + 32
-
-alert(`Цельсий: ${tempC}, Фаренгейт: ${tempF.toFixed(2)}`);
--- JavaScript/2.js
+++
@@ -1,9 +0,0 @@
-/*2. Cоздать функцию greeting, которая принимает имя и выводит приветствие, используя переданное имя, в консоль.*/
-
-function greeting (userName) 
-{
-    console.log(`Welcome, ${userName}`);
-}
-    
-const user = prompt("Enter your name: ");
-greeting(user);
